800bd158f0435f19bd25a718ec9f3800d6211e5d
witsplusx <wits_maotech@163.com>
2018-10-26 10:33:56 +0800

    去除backend

--- backend/api/controllers/AppController.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  serve: (req, res) => {
-    const fs = require('fs');
-
-    const app = __dirname + '/../../assets/index.html';
-    fs.createReadStream(app).pipe(res)
-  }
-};
--- backend/api/services/TokenService.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-const tokenSecret = 'secretissecret';
-
-module.exports = {
-  issue: payload => jwt.sign(payload, tokenSecret, {expiresIn: '7d'}),
-  verify: token => jwt.verify(token, tokenSecret)
-};
